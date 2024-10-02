34851e7215620a9315d1d5ba05e35f4b64df5b56
Mateus Mota Nobrega-MNOBRE <mnobre@motorola.com>
2024-10-02 10:07:58 -0400

    Intro layout stuff

--- app/src/main/java/com/example/deliveryapp/MainActivity.kt
+++ app/src/main/java/com/example/deliveryapp/MainActivity.kt
@@ -4,14 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.deliveryapp.ui.theme.DeliveryAppTheme
+import com.example.deliveryapp.ui.theme.Purple500
+import com.example.deliveryapp.ui.theme.Teal200
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +33,46 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DeliveryAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ProductItem()
             }
         }
     }
+
 }
 
+@Preview(showBackground = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Android $name!",
-        modifier = modifier
-    )
-}
+private fun ProductItem() {
 
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun GreetingPreview() {
-    DeliveryAppTheme {
-        Greeting("Android")
+    Column(
+        Modifier
+            .height(250.dp)
+            .width(200.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .height(100.dp)
+                .background(
+                    brush = Brush.horizontalGradient(
+                        colors = listOf(
+                            Purple500, Teal200
+                        )
+                    )
+                )
+                .fillMaxWidth()
+        )
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = null,
+            Modifier
+                .offset(y = (-50).dp)
+                .size(100.dp)
+                .clip(shape = CircleShape)
+                .align(Alignment.CenterHorizontally)
+        )
+        Text(text = "Texto 1")
+        Text(text = "Texto 2")
     }
 }
+
+
