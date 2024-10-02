51312cc1679f723df5deac7d73fd625f810557a8
Mateus Mota Nobrega-MNOBRE <mnobre@motorola.com>
2024-10-02 14:20:39 -0400

    Mais sobre layouts

--- app/src/main/java/com/example/deliveryapp/MainActivity.kt
+++ app/src/main/java/com/example/deliveryapp/MainActivity.kt
@@ -8,14 +8,18 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -32,6 +36,7 @@ import androidx.compose.ui.unit.sp
 import com.example.deliveryapp.ui.theme.DeliveryAppTheme
 import com.example.deliveryapp.ui.theme.Purple500
 import com.example.deliveryapp.ui.theme.Teal200
+import org.intellij.lang.annotations.PrintFormat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,7 +44,77 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DeliveryAppTheme {
-                ProductItem()
+                ProductsSection()
+            }
+        }
+    }
+
+}
+
+@Composable
+fun ProductsSection(modifier: Modifier = Modifier) {
+    Column {
+        Text(text = "Promoções")
+        Row {
+            ProductItem()
+            ProductItem()
+            ProductItem()
+        }
+    }
+}
+
+
+@Composable
+private fun ProductItem() {
+    Surface(
+        Modifier.padding(8.dp),
+        shape = RoundedCornerShape(15.dp),
+        tonalElevation = 4.dp
+    ) {
+        Column(
+            Modifier
+                .heightIn(250.dp, 300.dp)
+                .width(200.dp)
+        ) {
+            val imageSize = 100.dp
+            Box(
+                modifier = Modifier
+                    .height(imageSize)
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Purple500, Teal200
+                            )
+                        )
+                    )
+                    .fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = null,
+                    Modifier
+                        .offset(y = imageSize / 2)
+                        .size(imageSize)
+                        .clip(shape = CircleShape)
+                        .align(Alignment.BottomCenter)
+                )
+            }
+            Spacer(modifier = Modifier.height(imageSize / 2))
+
+            Column(Modifier.padding(16.dp)) {
+                Text(
+                    text = LoremIpsum(50).values.first(),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight(700),
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = "R$ 14,99",
+                    Modifier.padding(top = 8.dp),
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight(400)
+                )
             }
         }
     }
@@ -48,53 +123,13 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-private fun ProductItem() {
-
-    Column(
-        Modifier
-            .height(250.dp)
-            .width(200.dp)
-    ) {
-        Box(
-            modifier = Modifier
-                .height(100.dp)
-                .background(
-                    brush = Brush.horizontalGradient(
-                        colors = listOf(
-                            Purple500, Teal200
-                        )
-                    )
-                )
-                .fillMaxWidth()
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_launcher_background),
-                contentDescription = null,
-                Modifier
-                    .offset(y = 50.dp)
-                    .size(100.dp)
-                    .clip(shape = CircleShape)
-                    .align(Alignment.BottomCenter)
-            )
-        }
-        Spacer(modifier = Modifier.height(50.dp))
-
-        Column(Modifier.padding(16.dp)) {
-            Text(
-                text = LoremIpsum(50).values.first(),
-                fontSize = 18.sp,
-                fontWeight = FontWeight(700),
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis
-            )
-            Text(
-                text = "R$ 14,99",
-                Modifier.padding(top = 8.dp),
-                fontSize = 14.sp,
-                fontWeight = FontWeight(400)
-            )
-        }
-    }
+private fun ProductsSectionPreview() {
+    ProductsSection()
 }
 
+@Preview(showBackground = true)
+@Composable
+private fun ProductItemPreview() {
+    ProductItem()
+}
 
