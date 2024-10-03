578e9a8460a574ac671f5ce91a9b006bf635e304
Mateus Mota Nobrega-MNOBRE <mnobre@motorola.com>
2024-10-03 10:29:11 -0400

    recyvlerview horizontal and some other cleanup things

--- app/src/main/java/com/example/deliveryapp/MainActivity.kt
+++ app/src/main/java/com/example/deliveryapp/MainActivity.kt
@@ -3,13 +3,15 @@ package com.example.deliveryapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
@@ -17,15 +19,18 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -33,43 +38,94 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.deliveryapp.model.Product
 import com.example.deliveryapp.ui.theme.DeliveryAppTheme
 import com.example.deliveryapp.ui.theme.Purple500
 import com.example.deliveryapp.ui.theme.Teal200
-import org.intellij.lang.annotations.PrintFormat
+import java.math.BigDecimal
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            DeliveryAppTheme {
+            App()
+        }
+    }
+}
+
+@Composable
+fun App() {
+    DeliveryAppTheme {
+        Surface {
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                Spacer(Modifier)
                 ProductsSection()
+                ProductsSection()
+                ProductsSection()
+                Spacer(Modifier)
             }
         }
     }
-
 }
 
 @Composable
-fun ProductsSection(modifier: Modifier = Modifier) {
+fun ProductsSection() {
     Column {
-        Text(text = "Promoções")
-        Row {
-            ProductItem()
-            ProductItem()
-            ProductItem()
+        Text(
+            text = "Promoções",
+            Modifier.padding(
+                start = 16.dp,
+                end = 16.dp
+            ),
+            fontSize = 20.sp,
+            fontWeight = FontWeight(400)
+        )
+        Row(
+            Modifier
+                .padding(
+                    top = 8.dp
+                )
+                .fillMaxWidth()
+                .horizontalScroll(rememberScrollState()),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Spacer(Modifier)
+            ProductItem(
+                Product(
+                    name = "Hamburguer",
+                    price = BigDecimal("12.99"),
+                    image = R.drawable.burger
+                )
+            )
+            ProductItem(
+                Product(
+                    name = "Pizza",
+                    price = BigDecimal("19.99"),
+                    image = R.drawable.pizza
+                )
+            )
+            ProductItem(
+                Product(
+                    name = "Batata frita",
+                    price = BigDecimal("7.99"),
+                    image = R.drawable.fries
+                )
+            )
+            Spacer(Modifier)
         }
     }
 }
 
-
 @Composable
-private fun ProductItem() {
+fun ProductItem(product: Product) {
     Surface(
-        Modifier.padding(8.dp),
         shape = RoundedCornerShape(15.dp),
-        tonalElevation = 4.dp
+        shadowElevation = 4.dp
     ) {
         Column(
             Modifier
@@ -90,27 +146,27 @@ private fun ProductItem() {
                     .fillMaxWidth()
             ) {
                 Image(
-                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    painter = painterResource(id = product.image),
                     contentDescription = null,
                     Modifier
-                        .offset(y = imageSize / 2)
                         .size(imageSize)
+                        .offset(y = imageSize / 2)
                         .clip(shape = CircleShape)
-                        .align(Alignment.BottomCenter)
+                        .align(BottomCenter),
+                    contentScale = ContentScale.Crop
                 )
             }
             Spacer(modifier = Modifier.height(imageSize / 2))
-
             Column(Modifier.padding(16.dp)) {
                 Text(
-                    text = LoremIpsum(50).values.first(),
+                    text = product.name,
                     fontSize = 18.sp,
                     fontWeight = FontWeight(700),
                     maxLines = 2,
                     overflow = TextOverflow.Ellipsis
                 )
                 Text(
-                    text = "R$ 14,99",
+                    text = product.price.toPlainString(),
                     Modifier.padding(top = 8.dp),
                     fontSize = 14.sp,
                     fontWeight = FontWeight(400)
@@ -118,7 +174,12 @@ private fun ProductItem() {
             }
         }
     }
+}
 
+@Preview(showSystemUi = true)
+@Composable
+fun AppPreview() {
+    App()
 }
 
 @Preview(showBackground = true)
@@ -130,6 +191,11 @@ private fun ProductsSectionPreview() {
 @Preview(showBackground = true)
 @Composable
 private fun ProductItemPreview() {
-    ProductItem()
+    ProductItem(
+        Product(
+            name = LoremIpsum(50).values.first(),
+            price = BigDecimal("14.99"),
+            image = R.drawable.ic_launcher_background
+        )
+    )
 }
-
---
+++ app/src/main/java/com/example/deliveryapp/model/Product.kt
@@ -0,0 +1,12 @@
+package com.example.deliveryapp.model
+
+import android.media.Image
+import androidx.annotation.DrawableRes
+import java.math.BigDecimal
+
+class Product(
+    val name: String,
+    val price: BigDecimal,
+    @DrawableRes val image: Int
+)
+
